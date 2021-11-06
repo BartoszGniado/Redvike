fa3d4f20638440842168697ca887689fecec0d68
Bartosz Gniado <bartosz.gniado@gmail.com>
2021-11-07 00:29:04 +0100

    fix non-mutable mongoose Documents

--- services/reservations.js
+++ services/reservations.js
@@ -2,7 +2,7 @@ const Resevation = require('../db/models/resevation');
 const Amenity = require('../db/models/amenity');
 
 exports.getResevations = async ({ filters, sort, group }) => {
-  var resevations = await Resevation.find(filters).exec();
+  var resevations = await Resevation.find(filters).lean();
   if (sort)
     resevations = resevations.sort((a, b) =>
       sort.asc ? a[sort.by] - b[sort.by] : b[sort.by] - a[sort.by]
@@ -12,6 +12,11 @@ exports.getResevations = async ({ filters, sort, group }) => {
     const amenity = amenities[r['amenity_id']];
     r.amenity = amenity && amenity['name'];
     r.duration = r['end_time'] - r['start_time'];
+    // that's my first contact with mongodb
+    // finally we probably shouldn't duplicate id & _id
+    // internal db versioning __v shouldn't be returned ???
+    delete r._id;
+    delete r.__v;
   });
   if (group)
     resevations = resevations.reduce((acc, cur) => {
