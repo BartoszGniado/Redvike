178e63e5814187971ec7ac370e4bee46bbf9ba69
Bartosz Gniado <bartosz.gniado@gmail.com>
2021-11-27 16:06:01 +0000

    virtual field duration

--- db/models/reservation.js
+++ db/models/reservation.js
@@ -1,31 +1,45 @@
 const mongoose = require('mongoose');
 
-const reservationSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: true,
+const reservationSchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      required: true,
+    },
+    amenity: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'Amenity',
+      required: false,
+    },
+    user_id: {
+      type: Number,
+      required: true,
+    },
+    start_time: {
+      type: Number,
+      required: true,
+    },
+    end_time: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
   },
-  amenity: {
-    type: mongoose.SchemaTypes.ObjectId,
-    ref: 'Amenity',
-    required: false,
-  },
-  user_id: {
-    type: Number,
-    required: true,
-  },
-  start_time: {
-    type: Number,
-    required: true,
-  },
-  end_time: {
-    type: Number,
-    required: true,
-  },
-  date: {
-    type: Number,
-    required: true,
-  },
+  {
+    toObject: {
+      virtuals: true,
+    },
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+reservationSchema.virtual('duration').get(function () {
+  return this['end_time'] - this['start_time'];
 });
 
 module.exports = mongoose.model('Reservation', reservationSchema);
--- services/reservations.js
+++ services/reservations.js
@@ -2,16 +2,12 @@ const Reservation = require('../db/models/reservation');
 const Amenity = require('../db/models/amenity');
 
 exports.getReservations = async ({ filters, sort, group }) => {
-  var reservations = await Reservation.find(filters)
+  const reservations = await Reservation.find(filters)
     .populate('amenity')
-    .sort(sort)
-    .lean();
-  reservations.forEach((r) => {
-    r.duration = r['end_time'] - r['start_time'];
-    delete r.__v;
-  });
+    .sort(sort);
+  // TODO: group by db side
   if (group)
-    reservations = reservations.reduce((acc, cur) => {
+    return reservations.reduce((acc, cur) => {
       acc[cur[group.by]]
         ? acc[cur[group.by]].push(cur)
         : (acc[cur[group.by]] = [cur]);
